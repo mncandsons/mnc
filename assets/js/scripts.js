2deecc783eefebac75c2caf3befee10f60982b6d
Chermash Igor <chermash.igor@gmail.com>
2020-01-23 16:07:14 +0200

    Added scroll to children blocks, resolved adresses, added indicator

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -107,6 +107,25 @@ for(var i = 0; i < commercialRanges.length; i++){
     commercialRanges[i].setAttribute('data-index', i);
 }
 
+// Scroll Function
+const easeInCubic = function (t) { return t*t*t }
+
+const scrollToElem = (startTime, currentTime, duration, scrollEndElemTop, startScrollOffset) => {
+   const runtime = currentTime - startTime;
+   let progress = runtime / duration;
+
+   progress = Math.min(progress, 1);
+
+   const ease = easeInCubic(progress);
+
+   window.scroll(0, startScrollOffset + (scrollEndElemTop * ease));if(runtime < duration){
+     requestAnimationFrame((timestamp) => {
+       const currentTime = timestamp || new Date().getTime();
+       scrollToElem(startTime, currentTime, duration, scrollEndElemTop, startScrollOffset);
+     })
+   }
+ }
+
 // Portfolio blocks
 
 let pbElem = document.querySelectorAll('.p-block');
@@ -138,6 +157,17 @@ for(let i = 0; i < pbElem.length; i++){
           pbElem[i].parentNode.style.height = elemParentHeight + 'px';
           pbElem[i].parentNode.classList.remove('active');
         }
+
+        const anim = requestAnimationFrame((timestamp) => {
+          const stamp = timestamp || new Date().getTime();
+          const duration = 500;
+          const start = stamp;
+
+          const startScrollOffset = window.pageYOffset;
+          const scrollEndElemTop = elemSubBlocks.getBoundingClientRect().top -200;
+
+          scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
+        })
 
         elemParent.classList.add('active');
         pbBtn.textContent = 'Close';
@@ -250,23 +280,6 @@ for(let i = 0; i < scrollElems.length; i++){
    })
 })
 }
-const easeInCubic = function (t) { return t*t*t }
-
-const scrollToElem = (startTime, currentTime, duration, scrollEndElemTop, startScrollOffset) => {
-   const runtime = currentTime - startTime;
-   let progress = runtime / duration;
-
-   progress = Math.min(progress, 1);
-
-   const ease = easeInCubic(progress);
-
-   window.scroll(0, startScrollOffset + (scrollEndElemTop * ease));if(runtime < duration){
-     requestAnimationFrame((timestamp) => {
-       const currentTime = timestamp || new Date().getTime();
-       scrollToElem(startTime, currentTime, duration, scrollEndElemTop, startScrollOffset);
-     })
-   }
- }
 
 
 // Form Validation
