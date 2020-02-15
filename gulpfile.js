47819e030f965fb9d7f18c9aa5acc30ea0920ee1
Chermash Igor <chermash.igor@gmail.com>
2020-02-15 10:03:37 +0200

    Image testing

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const uglify = require('gulp-uglify-es').default;
 const concat = require('gulp-concat');
 const responsive = require('gulp-responsive');
 const rename = require('gulp-rename');
+const newer = require('gulp-newer');
 
 gulp.task('minify-js', () => {
     return gulp.src([
@@ -20,6 +21,7 @@ gulp.task('minify-js', () => {
 
 gulp.task('images', () => {
   return gulp.src('static/assets/images/*.{jpg,jpeg,png}')
+    .pipe(newer('./static/assets/images/public/'))
     .pipe(responsive({
       '**/*.{jpg,png,jpeg}': [{
         width: 2000,
@@ -43,6 +45,32 @@ gulp.task('images', () => {
     .pipe(gulp.dest('./static/assets/images/public/'));
 });
 
+gulp.task('images-prod', () => {
+  return gulp.src('/opt/build/cache/assets/images/*.{jpg,jpeg,png}')
+    .pipe(newer('/assets/images/public/'))
+    .pipe(responsive({
+      '**/*.{jpg,png,jpeg}': [{
+        width: 2000,
+        quality: 75,
+        compressionLevel: 7,
+      }, {
+        width: 2000,
+        quality: 75,
+        rename: {
+          extname: '.webp',
+        },
+      }],
+    }, {
+      errorOnEnlargement: false,
+      errorOnUnusedConfig: false
+    }))
+    .pipe(rename(function(opt) {
+      opt.basename = opt.basename.split(' ').join('_');
+      return opt;
+    }))
+    .pipe(gulp.dest('/assets/images/public/'));
+});
+
 gulp.task('hugo-build', shell.task(['hugo']))
 
 gulp.task('minify-html', () => {
@@ -57,4 +85,5 @@ gulp.task('minify-html', () => {
         .pipe(gulp.dest('./public'))
 })
 
-gulp.task('build', gulp.series('minify-js', 'images', 'hugo-build', 'minify-html'));
+gulp.task('build', gulp.series('minify-js', 'images-prod', 'hugo-build', 'minify-html'));
+gulp.task('build-local', gulp.series('minify-js', 'images', 'hugo-build', 'minify-html'));
