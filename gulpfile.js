bc702d1bef00dbf403cc8eb80b46fcd5ade8f65c
Chermash Igor <chermash.igor@gmail.com>
2020-06-11 15:34:38 +0300

    Test #2

--- gulpfile.js
+++ gulpfile.js
@@ -167,5 +167,5 @@ gulp.task('minify-html', () => {
         .pipe(gulp.dest('./public'))
 })
 
-gulp.task('build', gulp.series('minify-js', 'hugo-build', 'minify-html'));
+gulp.task('build', gulp.series('minify-js', 'images-prod', 'hugo-build', 'minify-html'));
 gulp.task('build-local', gulp.series('minify-js', 'images', 'hugo-build', 'minify-html'));
